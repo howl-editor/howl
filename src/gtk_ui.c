0cca4bfb241fbd87ff937bdc0fef2aef0d2e4bc4
Nils Nordman <nino@nordman.org>
2012-04-04 15:51:07 +0200

    Dont emit paint event, it's too noisy

--- src/gtk_ui.c
+++ src/gtk_ui.c
@@ -124,7 +124,7 @@ static gboolean on_sci_notify(GtkWidget *widget, gint ctrl_id, struct SCNotifica
   lua_newtable(L);
   set_nfield(L, "code", code);
 
-  if (code == SCN_PAINTED) {} /* fires a lot */
+  if (code == SCN_PAINTED) { return FALSE; } /* fires a lot */
   else if (code == SCN_UPDATEUI) {
     set_nfield(L, "updated", n->updated);
   }
