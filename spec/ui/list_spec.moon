howl-editor/howl

commit 80fa39716dd8227bf82685a89e44c873166317cd

Respect list.max_height more
Previously we cheated by ignoring eventual headers and captions.

lib/lunar/ui/list.moon
@@ -28,6 +28,16 @@ column_padding = (text, column, widths) ->
   return '' if not widths or column == #widths
   string.rep ' ', (widths[column] + 1) - #text
 
+line_count = (s) ->
+  count = -1
+  init = 0
+
+  while init
+    count += 1
+    _, init = s\find '\n', init + 1, true
+
+  count
+
 class List extends PropertyObject
   column_styles: { 'string', 'comment', 'operator' }
 
@@ -106,7 +116,7 @@ class List extends PropertyObject
     row = @_sel_row - 1
     row = #@items if row < 1
 
-    if row < @offset or row > @end_item
+    if row < @offset or row > @last_shown
       offset = row - @limit + 1
       @scroll_to math.max offset, 1
 
@@ -116,7 +126,7 @@ class List extends PropertyObject
     row = @_sel_row + 1
     row = 1 if row > #@items
 
-    if row < @offset or row > @end_item
+    if row < @offset or row > @last_shown
       @scroll_to row
 
     @select row
@@ -131,7 +141,7 @@ class List extends PropertyObject
     if @showing
       highlight.remove_all 'list_selection', @buffer
 
-      if row >= @offset and row <= @end_item
+      if row >= @offset and row <= @last_shown
         lines = @buffer.lines
         start_line = lines\nr_at_pos @start_pos
         sel_line = row - @offset + 1
@@ -146,11 +156,7 @@ class List extends PropertyObject
     @clear!
 
     total = #@items
-    @end_item = if @max_height
-        math.min @max_height + @offset - 1, total
-      else
-        total
-
+    lines_left = @max_height or math.huge
     pos = @start_pos
     buffer = @buffer
 
@@ -161,16 +167,23 @@ class List extends PropertyObject
     if @caption and lines_left > 0
       cap = @caption .. '\n'
       pos = buffer\insert cap, pos, 'list_caption'
+      lines_left -= line_count cap
 
-    if @headers and #@headers > 0
+    if @headers and #@headers > 0 and lines_left > 0
       for column, header in ipairs @headers
         padding = column_padding header, column, @_widths
         pos = buffer\insert header, pos, 'list_header'
         pos = buffer\insert padding, pos
 
       pos = buffer\insert '\n', pos
+      lines_left -= 1
 
-    for row = @offset, @end_item
+    @last_shown = if total > lines_left
+        math.min @offset + lines_left - 1, total
+      else
+       total
+
+    for row = @offset, @last_shown
       item = @items[row]
       start_pos = pos
       if @_multi_column
@@ -183,11 +196,11 @@ class List extends PropertyObject
 
       pos = buffer\insert '\n', pos
 
-    @nr_shown = @end_item - @offset + 1
+    @nr_shown = @last_shown - @offset + 1
 
-    if @nr_shown < total
+    if @nr_shown < total and lines_left > 0
       info = string.format '[..] (showing %d - %d out of %d)\n',
-        @offset, @end_item, total
+        @offset, @last_shown, total
       pos = @buffer\insert info, pos, 'comment'
 
     @_sel_row = @offset if not @_sel_row and @selection_enabled

spec/ui/list_spec.moon
@@ -66,10 +66,10 @@ first    item one
     list\show!
     assert_equal list.offset, 1
 
-  it '.end_item is set to the index of the last item shown', ->
+  it '.last_shown is set to the index of the last item shown', ->
     list.items = {'one', 'two', 'three'}
     list\show!
-    assert_equal list.end_item, 3
+    assert_equal list.last_shown, 3
 
   context 'when .offset is set', ->
     it 'shows items starting from offset', ->
@@ -97,7 +97,7 @@ first    item one
     assert_equal sci\get_current_pos!, #buf.text
 
   context 'when .max_height is set', ->
-    it 'with no headers it shows only up to max_height items', ->
+    it 'with only .items set it shows only up to max_height items', ->
       list.items = {'one', 'two', 'three'}
       list.max_height = 2
       list\show!
@@ -107,6 +107,22 @@ first    item one
       list.max_height = math.huge
       list\show!
       assert_equal buf.text, 'one\ntwo\nthree\n'
+
+    it 'it takes caption into account when set', ->
+      list.items = {'one', 'two'}
+      list.caption = 'Two\nliner'
+      list.max_height = 3
+      list\show!
+      assert_match 'one', buf.text
+      assert_not_match 'two', buf.text
+
+    it 'it takes headers into account when set', ->
+      list.items = {'one', 'two'}
+      list.headers = { 'Takes up one line' }
+      list.max_height = 2
+      list\show!
+      assert_match 'one', buf.text
+      assert_not_match 'two', buf.text
 
     it 'displays info about the currently shown items', ->
       list.items = {'one', 'two', 'three'}

commit 64e9b546280b0abbc54bde82ffe047e505483a86

Fix offset calculation when scrolling
When remaning nr of items are smaller than the max allowed nr of
visible lines.

Fallout from 80fa397.

lib/lunar/ui/list.moon
@@ -87,15 +87,15 @@ class List extends PropertyObject
     get: => @end_pos != nil
 
   scroll_to: (row) =>
-    error 'List is not shown', 2 if not @end_pos
+    error 'List is not shown', 2 if not @showing
 
     total = #@items
-    return if not @max_height or total < @max_height
+    max_item_lines = (@last_shown - @offset) + 1
     @offset = row
     if @offset < 1
       @offset = 1
-    elseif @offset > total or total - @offset < @max_height
-      @offset = total - @max_height + 1
+    elseif @offset > total or (total - @offset) + 1 < max_item_lines
+      @offset = (total - max_item_lines) + 1
 
     @show!
     @select row if @selection_enabled

spec/ui/list_spec.moon
@@ -303,11 +303,19 @@ three    four
       assert_match 'two', buf.text
       assert_match 'showing 2 %- 2 out of 3', buf.text
 
-    it 'adjust the actual offset to always show the same number of items', ->
-      list.max_height = 2
-      list\show!
-      list\scroll_to 3
-      assert_match 'two\nthree\n', buf.text
+    context 'when the remaining numbers are fewer than the max nr of visible items', ->
+      it 'adjust the actual offset to always show the same number of items', ->
+        list.max_height = 2
+        list\show!
+        list\scroll_to 3
+        assert_match 'two\nthree', buf.text
+
+      it 'accounts for headers when determining the new offset', ->
+        list.headers = { 'foo' }
+        list.max_height = 2
+        list\show!
+        list\scroll_to 3
+        assert_match '^foo\nthree', buf.text
 
     it 'selects <row> if selection is enabled', ->
       list.selection_enabled = true