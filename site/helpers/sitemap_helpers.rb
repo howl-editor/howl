0d5065bd76841928f3d81908b3994a3a473036db
Nils Nordman <nino@nordman.org>
2014-06-10 21:02:32 +0200

    Site: Splits API and spec links over three columns

--- site/helpers/sitemap_helpers.rb
+++ site/helpers/sitemap_helpers.rb
@@ -1,9 +1,39 @@
 module SitemapHelpers
   def howl_specs
-    sitemap.where(:tags.include => 'spec').all.sort_by(&:url)
+    grouped(sitemap.where(:tags.include => 'spec').all)
   end
 
   def howl_api_docs
-    sitemap.resources.select { |r| r.url.start_with? '/doc/api' }.sort_by(&:url)
+    grouped(sitemap.resources.select { |r| r.url.start_with? '/doc/api' })
+  end
+
+  def sliced_for_columns(nr_cols, packages)
+    header_size = 3
+    avg_length = packages.values.reduce(0) { |s, a| s + a.size } / nr_cols
+    longest = packages.values.map(&:size).max
+    goal_length = [avg_length, longest].max + header_size
+    col = []
+    length = 0
+
+    packages.each do |package, docs|
+      remaining = goal_length - length
+      new_length = length + docs.size + header_size
+      if new_length > goal_length and (new_length - goal_length) > remaining
+        yield col
+        col = []
+        length = 0
+      end
+      col << { package: package, docs: docs }
+      length += docs.size + header_size
+    end
+    yield col unless col.empty?
+  end
+
+  private
+
+  def grouped(docs)
+    docs.sort_by(&:url).group_by do |doc|
+      File.dirname(doc.url).tr('/', '.').gsub(/.doc.\w+/, 'howl')
+    end
   end
 end
