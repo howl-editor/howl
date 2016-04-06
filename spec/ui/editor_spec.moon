howl-editor/howl

commit 7cf076e18e18729f39896b0d3f4fb9cb077f1446

Change cursor behavior when unindenting line

lib/howl/ui/editor.moon
@@ -384,9 +384,10 @@ class Editor extends PropertyObject
 
     conf = @buffer.config
     if conf.tab_indents and @current_context.prefix.is_blank
+      cursor_col = @cursor.column
       cur_line = @current_line
       cur_line\unindent!
-      @cursor.column = cur_line.indentation + 1
+      @cursor.column = math.min(cursor_col, cur_line.indentation + 1)
     else
       return if @cursor.column == 1
       tab_stops = math.floor (@cursor.column - 1) / conf.tab_width
@@ -395,11 +396,12 @@ class Editor extends PropertyObject
       @cursor.column = col
 
   delete_back: =>
-    if @selection.empty and @current_context.prefix\match '^%s+$'
+    if @selection.empty and @current_context.prefix.is_blank
       if @buffer.config.backspace_unindents
         cur_line = @current_line
+        gap = cur_line.indentation - @cursor.column
         cur_line\unindent!
-        @cursor.column = cur_line.indentation + 1
+        @cursor.column = math.max(1, cur_line.indentation - gap)
         return
 
     @view\delete_back!

commit 870b51b1a4d5b660221a81e8b79d9f58127a59cf

Bugfix for backspace at start of line
Fix behavior broken by 7cf076e18e187 - special handling should happen
only if one or more spaces exist before cursor

lib/howl/ui/editor.moon
@@ -396,7 +396,8 @@ class Editor extends PropertyObject
       @cursor.column = col
 
   delete_back: =>
-    if @selection.empty and @current_context.prefix.is_blank
+    prefix = @current_context.prefix
+    if @selection.empty and prefix.is_blank and not prefix.is_empty
       if @buffer.config.backspace_unindents
         cur_line = @current_line
         gap = cur_line.indentation - @cursor.column

spec/ui/editor_spec.moon
@@ -470,6 +470,19 @@ describe 'Editor', ->
         editor\delete_back!
         assert.equal buffer.text, 'Əllo'
 
+      it 'deletes previous newline when at start of line', ->
+        config.backspace_unindents = false
+        buffer.text = 'hƏllo\nworld'
+        cursor.pos = 7
+        editor\delete_back!
+        assert.equal 'hƏlloworld', buffer.text
+
+        config.backspace_unindents = true
+        buffer.text = 'hƏllo\nworld'
+        cursor.pos = 7
+        editor\delete_back!
+        assert.equal 'hƏlloworld', buffer.text
+
       it 'unindents if in whitespace and backspace_unindents is true', ->
         config.indent = 2
         buffer.text = '  hƏllo'